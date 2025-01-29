188c1f48d16d4ae5bc053d4c46a6b65c3fc15bb9
Pavel Korobov <korobovpvl@gmail.com>
2025-01-29 13:12:16 +0300

    changed to test.each() structure

--- src/js/__tests__/char_display.test.js
+++ src/js/__tests__/char_display.test.js
@@ -1,29 +1,33 @@
 import { sortCharacters } from '../char_display';
 
 
-test('should return sorted array', () => {
-    let inputArray = [
-        {name: 'мечник', health: 10},
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-    ];
-    let expectedArray = [
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-        {name: 'мечник', health: 10},
-    ];
-
-    let result = sortCharacters(inputArray);
-    expect(result).toEqual(expectedArray);
-})
-
-test('should return the same array', () => {
-    let inputArray = [
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-        {name: 'мечник', health: 10},
-    ];
-
-    let result = sortCharacters(inputArray);
-    expect(result).toEqual(inputArray);
-})
+describe('char list sort function', () => {
+    test.each([
+        [
+            [
+                {name: 'мечник', health: 10},
+                {name: 'маг', health: 100},
+                {name: 'лучник', health: 80},
+            ],
+            [
+                {name: 'маг', health: 100},
+                {name: 'лучник', health: 80},
+                {name: 'мечник', health: 10},
+            ]   
+        ],
+        [
+            [
+                {name: 'маг', health: 100},
+                {name: 'лучник', health: 80},
+                {name: 'мечник', health: 10},
+            ],
+            [
+                {name: 'маг', health: 100},
+                {name: 'лучник', health: 80},
+                {name: 'мечник', health: 10},
+            ]
+        ]
+    ])('should return sorted list of char objects', (unsortedList, expectedList) => {
+        expect(sortCharacters(unsortedList)).toEqual(expectedList);
+    });
+});
